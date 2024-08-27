539fd579c354bdd536ee5fff14f915d3cb381c02
catalinfl <catalinfl02@gmail.com>
2024-08-27 04:09:45 +0300

    many errors added

--- errors.go
+++ errors.go
@@ -8,4 +8,7 @@ var (
 	ErrParseMultipartForm   = errors.New("error parsing multipart form")
 	ErrFileDoesntExist      = errors.New("file does not exist in form")
 	ErrUploadingPhoto       = errors.New("error uploading photo")
+	ErrNotAnImage           = errors.New("file is not an image")
+	ErrNotSameExtension     = errors.New("file and handler extension are not the same")
+	ErrDirDoesntExist       = errors.New("directory does not exist")
 )
